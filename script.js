670da1de5a872c92976d8b6c48f2eaa5da8525bf
Sujimax <sujithra1050@gmail.com>
2026-03-30 10:02:44 +0530

    Modified Skills section

--- script.js
+++ script.js
@@ -18,17 +18,38 @@ document.addEventListener("DOMContentLoaded", () => {
 const scrollTopBtn = document.getElementById("scrollTopBtn");
 
 window.addEventListener("scroll", () => {
-    if (window.scrollY > 300) {
-        scrollTopBtn.style.display = "block";
-    } else {
-        scrollTopBtn.style.display = "none";
-    }
+  if (window.scrollY > 300) {
+    scrollTopBtn.style.display = "block";
+  } else {
+    scrollTopBtn.style.display = "none";
+  }
 });
 
 scrollTopBtn.addEventListener("click", () => {
-    window.scrollTo({
-        top: 0,
-        behavior: "smooth"
-    });
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth"
+  });
 });
 
+const roles = [
+  "Full Stack Developer",
+  "Frontend Developer",
+  "Backend Developer"
+];
+
+let index = 0;
+const roleText = document.getElementById("role-text");
+
+function changeRole() {
+  index = (index + 1) % roles.length;
+
+  roleText.style.opacity = 0;
+
+  setTimeout(() => {
+    roleText.textContent = roles[index];
+    roleText.style.opacity = 1;
+  }, 300);
+}
+
+setInterval(changeRole, 2000);
